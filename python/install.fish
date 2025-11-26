653b15c4235af57970c57e5d79aa178868f50530
stvmachine <campos.esteban@gmail.com>
2025-11-26 13:16:39 +1100

    feat(python): add Python component
    Create dedicated python component:
    - python/install.fish: Pyenv setup (commented template)
    - python/conf.d/aliases.fish: Python and pip aliases
    
    Moves Python configuration from starship.fish to modular component structure.

---
+++ python/install.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/env fish
+# Python and Pyenv setup
+
+# Pyenv configuration (commented out - uncomment if needed)
+# set -g PYENV_ROOT $HOME/.pyenv
+# fish_add_path $PYENV_ROOT/bin:$PATH
+# alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
+
+# If everything fails, run this in the command line to make it works 'python' and 'pip' commands
+# eval "$(pyenv init --path)"
+
+# set if your term supports `pipenv shell --fancy`
+# set pipenv_fish_fancy yes
+
