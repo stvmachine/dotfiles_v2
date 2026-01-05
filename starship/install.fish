05460d6a64637a111c5005d14e9c1d73c0c2d3b6
stvmachine <campos.esteban@gmail.com>
2026-01-06 10:28:22 +1100

    fix: properly link Starship config file in install script
    - Add link_file call to symlink starship config to ~/.config/starship.toml
    - Ensures Starship configuration is properly loaded

--- starship/install.fish
+++ starship/install.fish
@@ -1,2 +1,5 @@
 #!/usr/bin/env fish
 set -Ux STARSHIP_CONFIG $DOTFILES/starship/config.toml
+
+# Link the starship config file
+link_file $DOTFILES/starship/config.toml ~/.config/starship.toml backup
