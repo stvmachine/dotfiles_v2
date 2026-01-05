32e9a5323286a6d507fbd057e2afb01802dae7de
stvmachine <campos.esteban@gmail.com>
2026-01-06 10:50:50 +1100

    feat: add comprehensive fzf configuration for fish
    - Configure FZF_DEFAULT_COMMAND to use fd or find
    - Set FZF_DEFAULT_OPTS with preview, border, and clipboard support
    - Add enhanced history search with Ctrl+R binding
    - Provide fallbacks for missing tools

--- system/install.fish
+++ system/install.fish
@@ -1,3 +1,37 @@
 #!/usr/bin/env fish
 abbr -a less 'less -r'
 
+# fzf configuration
+if command -qs fzf
+    # Use fd for fzf if available, otherwise use find
+    if command -qs fd
+        set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+    else
+        set -gx FZF_DEFAULT_COMMAND 'find . -type f -not -path "*/\.git/*"'
+    end
+
+    # fzf options
+    set -gx FZF_DEFAULT_OPTS '
+        --height 40%
+        --layout=reverse
+        --border
+        --preview "bat --style=numbers --color=always {} || cat {}"
+        --preview-window right:60%:wrap
+        --bind "ctrl-y:execute-silent(echo {} | pbcopy)+abort"
+    '
+
+    # Use fzf for better history search
+    if command -qs fzf
+        function history-search
+            history | fzf --tac | read -l command
+            if test $command
+                commandline -rb $command
+            end
+        end
+
+        function fish_user_key_bindings
+            bind \cr history-search
+        end
+    end
+end
+
