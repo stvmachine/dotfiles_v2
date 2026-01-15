dbdda8cdad13119296151da6fd93fd7231bc0dec
stvmachine <campos.esteban@gmail.com>
2026-01-16 10:46:08 +1100

    feat(node): add support for .node-version files
    The nvm auto-switch now checks for .node-version in addition to .nvmrc,
    with .nvmrc taking priority if both exist.

--- node/conf.d/nvm.fish
+++ node/conf.d/nvm.fish
@@ -9,16 +9,24 @@ if command -qs brew
         nvm use default --silent 2>/dev/null
     end
 
-    # Trigger 'nvm use' if .nvmrc exists in the folder
+    # Trigger 'nvm use' if .nvmrc or .node-version exists in the folder
     function __check_nvm --on-variable PWD --description 'Do nvm stuff'
+      # Check for .nvmrc first, then .node-version
+      set -l version_file ""
       if test -f .nvmrc
+        set version_file .nvmrc
+      else if test -f .node-version
+        set version_file .node-version
+      end
+
+      if test -n "$version_file"
         set node_version (nvm version)
-        set nvmrc_node_version (nvm version (cat .nvmrc))
+        set nvmrc_node_version (nvm version (cat $version_file))
 
         if [ $nvmrc_node_version = "N/A" ]
-          nvm install
+          nvm install (cat $version_file)
         else if [ $nvmrc_node_version != $node_version ]
-          nvm use
+          nvm use (cat $version_file)
         end
 
         # Testing if ~/bin folder exists
@@ -40,3 +48,4 @@ if command -qs brew
     __check_nvm
 end
 
+set -Ux NODE_OPTIONS "--max-old-space-size=4096"
