48d572bb5919871adba132fbe7d273b8def36fd0
stvmachine <campos.esteban@gmail.com>
2026-02-16 14:11:13 +1100

    ♻️ refactor(ruby): move rbenv init to conf.d for shell startup
    - Move rbenv PATH and init setup to conf.d/rbenv.fish so it runs on every terminal start
    - Simplify install.fish to bootstrap-only placeholder
    - Fix indentation in __check_ruby_version function

--- ruby/conf.d/rbenv.fish
+++ ruby/conf.d/rbenv.fish
@@ -1,10 +1,18 @@
+# Only runs if rbenv is installed
+if test -d ~/.rbenv
+    set -gx RBENV_ROOT $HOME/.rbenv
+    fish_add_path $RBENV_ROOT/bin 2>/dev/null; or true
+
+    # Init rbenv with fish
+    status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source 2>/dev/null; or true
+end
+
 # Trigger 'rbenv install x.y.z' if .ruby-version exists in the folder
 function __check_ruby_version --on-variable PWD --description 'Do rbenv stuff'
-  if test -f .ruby-version
-    yes no | rbenv install (cat .ruby-version)
-  end
+    if test -f .ruby-version
+        yes no | rbenv install (cat .ruby-version)
+    end
 end
 
 # Install ruby version if it's necessary
 __check_ruby_version
-
--- ruby/install.fish
+++ ruby/install.fish
@@ -1,11 +1,3 @@
 #!/usr/bin/env fish
-# Ruby and rbenv setup
-
-# Only runs if rbenv is installed
-if test -d ~/.rbenv
-    set -g RBENV_ROOT $HOME/.rbenv
-    fish_add_path $RBENV_ROOT/bin 2>/dev/null; or true
-
-    # Init rbenv with fish
-    status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source 2>/dev/null; or true
-end
+# Ruby and rbenv bootstrap (one-time setup)
+# Runtime initialization is handled by conf.d/rbenv.fish
